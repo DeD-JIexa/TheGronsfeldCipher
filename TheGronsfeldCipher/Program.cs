f50d3d862728efd1add05a788b9386f00319a30c
Alex <alex3kot@outlook.com>
2020-06-29 09:17:36 +0400

    Добавьте файлы проекта.

---
+++ TheGronsfeldCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TheGronsfeldCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ";
+            int[] keys = "2718".Select(ch => (int)Char.GetNumericValue(ch)).ToArray();
+            Console.WriteLine("Введите ваше слово:");
+            string s =  Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                sb.Append(alphabet[(alphabet.IndexOf(s[i]) + keys[i % keys.Length]) % alphabet.Length]);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
